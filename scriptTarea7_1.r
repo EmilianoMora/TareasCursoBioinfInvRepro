1e486068accdc5735083f5ed1862d19dcc24743a
EmilianoMora <36041423+EmilianoMora@users.noreply.github.com>
2018-04-23 18:44:26 -0500

    Se agrega Script de Tarea 7_1

---
+++ scriptTarea7_1.r
@@ -0,0 +1,43 @@
+# TareasCursoBioinfInvRepro
+#este es un script para hacer la pregunta 1 de la Tarea 7.1 del curso de Bioinformatica ...
+
+
+#Primero vamos a establecer el directorio de trabajo 
+setwd ("~/BioinfinvRepro/Unidad7/Prac_Uni7/maices/meta")
+
+#1) cargamos la base de datos ""maizteocintle_SNP50k_meta_extended.txt" y lo dirigimos a un archivo que se llama "fullmat"
+
+fullmat <- read.delim("maizteocintle_SNP50k_meta_extended.txt", header = TRUE)
+
+#2) determinamos que la base de datos es un data frame
+
+class(fullmat)
+
+#3) vemos cuales son las primeras 6 lineas de nuetra base de datos
+
+head (fullmat, n=6)
+
+#4) determinamos el numero de muestras que existen en nuetra base de datos es 165 
+
+
+#5) determinamos que se tienen observaciones de 19 estados
+
+str(fullmat)
+
+#6) determinamos que el numero de muestras colectadas antes de 1980 es  
+
+length(which(fullmat$A.o._de_colecta < 1980))
+
+#7) para determinar la cantidad de muetsars de cada una de las razas usamos la funcion "summary"
+
+summary(fullmat$Raza)
+
+#8) determinamos que en promedio las muestars fueron colectadas a metros sobre el nivel del mar
+
+mean (fullmat$Altitud)
+
+#9) determinamos que la altura maxima y minima de colecta fueron 
+
+max(fullmat$Altitud)
+min(fullmat$Altitud)
+
